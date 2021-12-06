bc5ca3f8fd02a6e5313bdcfa78b7293f7f91629b
Marv <77222725+marv-24@users.noreply.github.com>
2021-12-06 13:21:39 -0500

    Update search.php

--- search.php
+++ search.php
@@ -15,52 +15,52 @@
 <body>
   <center>
     <h1>Create Final List</h1>
+    <form action="send-form.php" method='post'>
   <?php
-  
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+
   // Create connection
-  include 'db.php';
-  $conn = connect();
-  
+  $conn = mysqli_connect($servername, $username, $password);
+  $db = mysqli_select_db($conn, 'Bookstore');
   if(isset($_POST['search'])){
 
     $name = $_POST['Username'];
     $sem = $_POST['Semester'];
 
-    $sql = "SELECT forms_list.FacultyUsername, forms_list.FormID,
-            forms_list.Semester, individual_forms.FormID,
-            individual_forms.Class, individual_forms.Title,
-            individual_forms.Authors, individual_forms.Edition,
-            individual_forms.Publisher, individual_forms.ISBN
+    $sql = "SELECT *
             FROM forms_list, individual_forms
             WHERE forms_list.FacultyUsername = '$name' AND forms_list.Semester = '$sem'
             AND forms_list.FormID = individual_forms.FormID ";
 
     $query_run = mysqli_query($conn, $sql);
 
-    while($row = mysqli_fetch_array($query_run)){
+    $i = 0;
+    for($i = 1;$row = mysqli_fetch_array($query_run); $i++){
       ?>
-      <form action="send-form.php" method='post'>
-        <input type='text' name = 'FormID' value='<?php echo $row['FormID'] ?>'/><br>
-		<input type='text' name = 'Username' value='<?php echo $row['FacultyUsername'] ?>'/><br>
-		<input type='text' name = 'Semester' value='<?php echo $row['Semester'] ?>'/><br>
-        <input type='text' name = 'Class' value='<?php echo $row['Class'] ?>'/><br>
-        <input type='text' name = 'Title' value='<?php echo $row['Title'] ?>'/><br>
-        <input type='text' name = 'Authors' value='<?php echo $row['Authors'] ?>'/><br>
-        <input type='text' name = 'Edition' value='<?php echo $row['Edition'] ?>'/><br>
-        <input type='text' name = 'Publisher' value='<?php echo $row['Publisher'] ?>'/><br>
-        <input type='text' name = 'ISBN' value='<?php echo $row['ISBN'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Username'?> value='<?php echo $row['FacultyUsername'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Semester'?> value='<?php echo $row['Semester'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'FormID'?> value='<?php echo $row['FormID'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Class'?> value='<?php echo $row['Class'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Title'?> value='<?php echo $row['Title'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Authors'?> value='<?php echo $row['Authors'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Edition'?> value='<?php echo $row['Edition'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'Publisher'?> value='<?php echo $row['Publisher'] ?>'/><br>
+        <input type='text' name = <?php echo $i. 'ISBN'?> value='<?php echo $row['ISBN'] ?>'/><br>
       <?php
     }
 
   }
   ?>
-        <input type="submit" name='search' value="Submit Form"><br>
-      </form>
-    
-    <form action="CreateFinal.php">
-      <br>
-      <input type="submit" name='return' value="Return">
-    </form>
+  <input type = 'hidden' name = 'i' value = <?php echo $i ?>>
+  <input type="submit" name='search' value="Submit Form"><br>
+</form>
+
+  <form action="CreateFinal.php">
+    <br>
+    <input type="submit" name='return' value="Return">
+  </form>
 
 </center>
 </body>
