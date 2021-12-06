7ad4e1a76855884df02ff3e89077a87ba5566413
Michael Sedlack <Michael.sedlack@knights.ucf.edu>
2021-12-06 12:04:36 -0500

    CreateFinal now auto-fills the username to be the logged in user and displays the semesters as a dropdown menu

--- CreateFinal.php
+++ CreateFinal.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+	include 'db.php';
+	$link = connect();
+?>
 <!DOCTYPE html>
 <html lang = 'en'>
 <head>
@@ -21,11 +26,22 @@
   <h1>Create Final List</h1>
   <form action="search.php" method="POST">
     Please enter your Username<br>
-    <input type="text" name="Username" placeholder="Username" >
+    <input type="hidden" name="Username" placeholder="Username" value=<?php echo $_SESSION['username']?>>
     <br>
-    <input type="text" name="Semester" placeholder="Semester" >
+    <select name="Semester">
+		<?php
+			$query="SELECT Semester FROM forms_list WHERE FacultyUsername = '".$_SESSION['username']."'";
+			$result=mysqli_query($link, $query);
+			while($rows=mysqli_fetch_array($result, MYSQLI_ASSOC))
+			{
+		?>
+			<option value=<?php echo $rows['Semester'] ?>><?php echo $rows['Semester'] ?></option>
+		<?php
+			}
+		?>
+	</select>
     <br>
-    <input type="submit" name='search' value="Search Form">
+    <input type="submit" name='search'>
   </form>
 
 </center>
