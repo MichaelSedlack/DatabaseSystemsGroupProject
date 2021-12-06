1850ebb2a28475831f417bdf6259634e47726fe8
Marv <77222725+marv-24@users.noreply.github.com>
2021-12-06 13:08:46 -0500

    Update send-form.php

--- send-form.php
+++ send-form.php
@@ -15,33 +15,39 @@
 <body>
   <center>
     <?php
-    include 'db.php';
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
 
     // Create connection
-    $conn = connect();
+    $conn = mysqli_connect($servername, $username, $password);
+    $db = mysqli_select_db($conn, 'Bookstore');
 
-    $name = $_POST['Username'];
-    $id = $_POST['FormID'];
-    $sem = $_POST['Semester'];
-    $class = $_POST['Class'];
-    $title = $_POST['Title'];
-    $author = $_POST['Authors'];
-    $ed = $_POST['Edition'];
-    $pub = $_POST['Publisher'];
-    $is = $_POST['ISBN'];
+    $i = $_POST["i"];
+    for($j = 1; $j < $i; $j++)
+    {
+      $name = $_POST[$j.'Username'];
+      $id = $_POST[$j.'FormID'];
+      $sem = $_POST[$j.'Semester'];
+      $class = $_POST[$j.'Class'];
+      $title = $_POST[$j.'Title'];
+      $author = $_POST[$j.'Authors'];
+      $ed = $_POST[$j.'Edition'];
+      $pub = $_POST[$j.'Publisher'];
+      $is = $_POST[$j.'ISBN'];
 
-    $sql = "INSERT INTO Store (FacultyUsername, FormID, Semester, Class, Title, Authors, Edition, Publisher, ISBN)
-    VALUES ('$name', '$id', '$sem', '$class', '$title', '$author', '$ed', '$pub', '$is' )";
+      $sql = "INSERT INTO Store (FacultyUsername, FormID, Semester, Class, Title, Authors, Edition, Publisher, ISBN)
+      VALUES ('$name', '$id', '$sem', '$class', '$title', '$author', '$ed', '$pub', '$is' )";
 
-    if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully";
-    }
-    else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
+      if ($conn->query($sql) === TRUE) {
+        if($j == $i - 1)
+          echo "Form Sent Successfully !!!";
+      }
+      else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+  }
     ?>
-    
     <form action="CreateFinal.php">
       <br>
       <input type="submit" name='return' value="Return">
